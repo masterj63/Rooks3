e2be528c3c5016125f39a4539739ccfc663a92d6
Igor B <masterj63@gmail.com>
2016-01-29 19:25:47 +0400

    Add Partial things.

---
+++ src/main/java/ru/samsu/mj/board/PartialComparation.java
@@ -0,0 +1,5 @@
+package ru.samsu.mj.board;
+
+public enum PartialComparation {
+    LESS, EQUALS, GREATER, INCOMPARABLE
+}
---
+++ src/main/java/ru/samsu/mj/board/PartiallyComparable.java
@@ -0,0 +1,5 @@
+package ru.samsu.mj.board;
+
+public interface PartiallyComparable<E> {
+    public PartialComparation partiallyCompare(E e);
+}
