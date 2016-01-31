dedb2a682f7ec7e5aafd09d2671f1f8406597da1
Igor B <masterj63@gmail.com>
2016-02-01 02:39:47 +0400

    Add BoardMatrix.

---
+++ src/main/java/ru/samsu/mj/board/BoardMatrix.java
@@ -0,0 +1,25 @@
+package ru.samsu.mj.board;
+
+class BoardMatrix {
+    private final boolean[][] MATRIX;
+
+    private BoardMatrix(boolean[][] matrix) {
+        MATRIX = matrix;
+    }
+
+    static BoardMatrix valueOfDeflatedBoard(int[] deflatedBoard) {
+        boolean[][] matrix = new boolean[deflatedBoard.length][];
+
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = new boolean[i];
+            if (deflatedBoard[i] > 0)
+                matrix[i][deflatedBoard[i]] = true;
+        }
+
+        return new BoardMatrix(matrix);
+    }
+
+    boolean get(int i, int j) {
+        return MATRIX[i][j];
+    }
+}
