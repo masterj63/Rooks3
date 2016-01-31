a3ccb0938fca6d8be704633af54e49a9fac261b4
Igor B <masterj63@gmail.com>
2016-02-01 02:41:29 +0400

    Fix access modifier for BoardGenerator.

--- src/main/java/ru/samsu/mj/board/BoardGenerator.java
+++ src/main/java/ru/samsu/mj/board/BoardGenerator.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BoardGenerator {
+class BoardGenerator {
     public static BoardCollection generateByDimension(int n) {
         List<Board> list = new ArrayList<>();
         int[] board = new int[n];
